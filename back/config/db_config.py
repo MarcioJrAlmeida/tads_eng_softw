56e67785179dfb8bf10af87608226c35fc14a300
marcio <marciojr.almeida96@gmail.com>
2025-06-21 14:19:02 -0300

    Configuração do BackEnd com o Banco de Dados

---
+++ back/config/db_config.py
@@ -0,0 +1,9 @@
+# back/config/db_config.py
+
+db_params = {
+    "server": "localhost\\SQLEXPRESS",
+    "database": "ifpe_sado",
+    "username": "bd_sado",
+    "password": "bd_admin321@",
+    "driver": "ODBC Driver 17 for SQL Server"
+}
